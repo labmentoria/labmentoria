f8710f6a05f35b0fe04c93cace75b15df6f1e51a
Henrique Rodrigues <hjdesigner23@gmail.com>
2016-12-11 18:36:14 -0200

    Ajuste no javascript que abre o modal do login

--- src/script/components/modal.js
+++ src/script/components/modal.js
@@ -1,26 +1,36 @@
-//Colocando os elementos em uma variavel para fazer a função do click
-var btnLogin  = document.getElementById('nav-login');
-var overlay   = document.getElementById('overlay');
-var btnFechar = document.getElementById('fechar-modal');
+function loginModal(){
+  //Colocando os elementos em uma variavel
+  var btnLogin  = document.getElementById('nav-login');
+  var overlay   = document.getElementById('overlay');
+  var btnFechar = document.getElementById('fechar-modal');
+  var modalLogin  = document.getElementById('modal-login');
 
-//Colocando os elementos em uma variavel para fazer os efeitos
-var modalLogin  = document.getElementById('modal-login');
+  //Abrindo modal de login
+  if (document.getElementById('nav-login')) {
+    btnLogin.addEventListener('click', function(){
+      event.preventDefault();
+      overlay.classList.add('overlay-active');
+      modalLogin.classList.add('modal-active');
+    });
+  }
+  //Fechando modal de login quando click no fundo preto
+  if (document.getElementById('overlay')) {
+    overlay.addEventListener('click', function(){
+      event.preventDefault();
+      overlay.classList.remove('overlay-active');
+      modalLogin.classList.remove('modal-active');
+    });
+  }
+  //Fechando o modal de login quando clica no x
+  if (document.getElementById('fechar-modal')) {
+    btnFechar.addEventListener('click', function(){
+      event.preventDefault();
+      overlay.classList.remove('overlay-active');
+      modalLogin.classList.remove('modal-active');
+    });    
+  }
+}
 
-//Abrindo modal de login
-btnLogin.addEventListener('click', function(){
-  event.preventDefault();
-  overlay.className += ' overlay-active';
-  modalLogin.className += ' modal-active';
-});
-//Fechando modal de login quando click no fundo preto
-overlay.addEventListener('click', function(){
-  event.preventDefault();
-  overlay.classList.remove('overlay-active');
-  modalLogin.classList.remove('modal-active');
-});
-//Fechando o modal de login quando clica no x
-btnFechar.addEventListener('click', function(){
-  event.preventDefault();
-  overlay.classList.remove('overlay-active');
-  modalLogin.classList.remove('modal-active');
-});
+window.onload = function(){
+  loginModal();
+}
