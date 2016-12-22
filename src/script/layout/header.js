81fde1e1f3e718edf25cdc38090e44eb5cb7721f
Henrique Rodrigues <henrique.rodrigues@profite.com.br>
2016-12-22 12:19:29 -0200

    Finalizei o responsivo da home, header e footer

--- src/script/layout/header.js
+++ src/script/layout/header.js
@@ -1,4 +1,3 @@
-var navAccount;
 var headerLogin;
 
 function nav(){
@@ -8,8 +7,6 @@ function nav(){
     //Colocando os elementos  de click em uma variavel
     navMobile  = document.getElementById('nav-mobile');
     headerMenu  = document.getElementById('header-menu');
-    navAccount = document.getElementById('nav-login-mobile');
-    headerLogin = document.getElementById('header-login');
 
     //Clicando para aparecer o menu
     navMobile.addEventListener('click', function(event){
@@ -21,8 +18,6 @@ function nav(){
         headerMenu.classList.remove('nav-active');        
       }else{
         //Se não existir coloca e mostra o menu
-        navAccount.classList.remove('account-active');
-        headerLogin.classList.remove('account-active');
         navMobile.classList.add('nav-active');
         headerMenu.classList.add('nav-active');
       }
@@ -31,35 +26,6 @@ function nav(){
 
 }
 
-function account(){
-  //Verificando se exite o elemento
-  if(document.getElementById('nav-login-mobile')){
-    //Colocando o elemento de click numa variavel
-    navAccount = document.getElementById('nav-login-mobile');
-    headerLogin = document.getElementById('header-login');
-    navMobile  = document.getElementById('nav-mobile');
-    headerMenu  = document.getElementById('header-menu');
-
-    //Clicando para aparecer o menu de login
-    navAccount.addEventListener('click', function(event){
-      event.preventDefault();
-      //Verificando se exite a class no elemento
-      if(navAccount.classList.contains('account-active')){
-        //Se existe, remove ela e esconde o login
-        navAccount.classList.remove('account-active');
-        headerLogin.classList.remove('account-active');
-      }else{
-        //Se não existir coloca e mostra o login
-        navMobile.classList.remove('nav-active');
-        headerMenu.classList.remove('nav-active');
-        navAccount.classList.add('account-active');
-        headerLogin.classList.add('account-active');
-      }
-    });
-  }
-}
-
 window.onload = function(){
   nav();
-  account();
 }
