f4958185e0c0176267b5ed3ca247ec6085b0102a
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-01-14 23:45:18 -0200

    Finalizado a pagina de pupilos e criei dois campos no banco de dados. Cidade e Estado

--- src/infra/UserDao.js
+++ src/infra/UserDao.js
@@ -3,7 +3,7 @@ function UserDAO(connection,user_id){
 	this._user_id = user_id;
 }
 UserDAO.prototype.pupilo = function(callback){
-	this._connection.query('SELECT * FROM pupilo WHERE id='+this._user_id+'',callback);
+	this._connection.query('SELECT p.*, c.nome AS categoria FROM pupilo AS p  INNER JOIN categoria AS c ON p.idCategoria = c.id WHERE p.id='+this._user_id+' ORDER BY p.id ASC',callback);
 }
 module.exports = function(){
 	return UserDAO;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,6 @@
 module.exports = function(app){
 	app.get('/user',function(req,res){
 		var user_id = req.param('id');
-		console.log(user_id);
 		var connection = app.infra.connectionFactory();
 		var UserDao = new app.infra.UserDao(connection,user_id);
 		UserDao.pupilo(function(erros,resultados){
